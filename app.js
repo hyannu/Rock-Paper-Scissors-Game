8ad3b3b808f7a642d2104b649a09c6412401ccd4
hyannu <annurao9560@gmail.com>
2026-07-02 18:17:50 +0530

    add initial game logic and choice handling

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 let userScore = 0;
 let compScore = 0;
+
+
+const choices = document.querySelectorAll(".choice");
+
+ const genCompChoice = () => {
+    const options =["rock", "paper", "scissors"];
+    Math.random()
+   //rock,paper,scissors
+
+ }
+
+
+const playGame (userChoice) => {
+    console.log("user choice = ", userChoice);
+    //Generate computer choice--> modular 
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", ()=> {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
